d3c93104d85d72a50b1d50ead356796281048b55
CristianICh <spikecarp@gmail.com>
2023-10-06 15:11:19 -0300

    first commit

---
+++ js/app.js
@@ -0,0 +1,59 @@
+let url = "https://fakestoreapi.com/products";
+
+
+
+
+fetch(url)
+    .then(res => res.json())
+    .then(json => mostrarProductos(json));
+
+
+
+var categorias = document.getElementById('lista-categorias');
+console.log(categorias);
+
+function obtenerCategoria(e) {
+    var categoria=e.target.innerText;
+    console.log(categoria);
+    if(categoria!=""){
+        fetch(url+"/category/"+categoria)
+        .then(res=>res.json())
+        .then(json=>mostrarProductos(json))
+    }
+    
+}
+categorias.addEventListener('click', obtenerCategoria)
+const mostrarProductos = (json) => {
+    console.log(json);
+
+    let card = "";
+
+    for (let i = 0; i < json.length; i++) {
+        // card+=`<img src="${json[i].image}">`
+        card += ` 
+        <div class="card">
+        <a href="producto.html">
+             <img src="${json[i].image}" id="${json[i].id}">
+             </a>
+         <div class="desc">
+             <h4>${json[i].title}</h4>
+             <h5>$${json[i].price}</h5>
+         </div>
+        </div> 
+        `
+
+    }
+    document.getElementById('img').innerHTML = card;
+}
+var link = document.getElementById('img');
+var idencontrado;
+
+function encontrarId(e) {
+    console.log(e.target.id);
+    idencontrado = e.target.id;
+    console.log(idencontrado);
+    localStorage.setItem("id", idencontrado)
+
+
+}
+link.addEventListener('click', encontrarId);
