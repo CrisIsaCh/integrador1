158304f57ae71454373e62cc1deb568d5a226c5d
CristianICh <spikecarp@gmail.com>
2023-10-07 18:26:17 -0300

    categoria

--- js/app.js
+++ js/app.js
@@ -9,11 +9,12 @@ fetch(url)
 
 
 
+
 var categorias = document.getElementById('lista-categorias');
 console.log(categorias);
 
 function obtenerCategoria(e) {
-    var categoria=e.target.innerText;
+    var categoria=e.target.title;
     console.log(categoria);
     if(categoria!=""){
         fetch(url+"/category/"+categoria)
