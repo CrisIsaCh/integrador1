7870d7dd04120d8419e4b2671ca35c28d058a7c0
CristianICh <spikecarp@gmail.com>
2023-10-11 00:56:44 -0300

    se activo la seleccion cat inicio

--- js/app4.js
+++ js/app4.js
@@ -1,1 +1,39 @@
-console.log(localStorage.getItem("e") );
+console.log(localStorage.getItem("e"));
+
+let categoria = localStorage.getItem("e");
+
+
+let url = "https://fakestoreapi.com/products";
+
+var mostrarxcategoria=()=>{
+    return fetch(url + "/category/" + categoria)
+    .then(res => res.json())
+    .then(json => mostrarProductos(json))
+}
+
+
+fetch(url + "/category/" + categoria)
+    .then(res => res.json())
+    .then(json => mostrarProductos(json))
+
+var categorias = document.getElementById('lista-categorias');
+console.log(categorias);
+
+function obtenerCategoria(e,mostrarxcategoria) {
+    var categoria = e.target.title;
+    console.log(categoria);
+    if (categoria != "") {
+        fetch(url + "/category/" + categoria)
+            .then(res => res.json())
+            .then(json => mostrarProductos(json))
+        
+    } else {
+        fetch(url)
+        .then(res => res.json())
+        .then(json => mostrarProductos(json))
+    }
+
+}
+categorias.addEventListener('click', obtenerCategoria)
+
+
